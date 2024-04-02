35f9a85e78ac4fa74d77435e8ebf8f8d09a32ee3
boon2187 <fumiya.mai.ito@gmail.com>
2024-04-02 13:19:56 +0900

    ちゃんとTodoを追加できるようになったので、todosの初期値を空の配列にした。

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import TodoList from './conponents/TodoList';
 
 function App() {
   // Todoを保持するstateを作成
-  const [todos, setTodos] = useState([{id:"1", task:"todo1", isFinished:false},{id:"2", task:"todo2", isFinished:false},{id:"3", task:"todo3", isFinished:false}]);
+  const [todos, setTodos] = useState([]);
 
   // Todoを追加する関数
   const addTodo = (addingtask) => {
