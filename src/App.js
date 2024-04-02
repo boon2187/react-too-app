9762d54d306574d8f503a6bdb37f654716f5190c
boon2187 <fumiya.mai.ito@gmail.com>
2024-04-02 11:56:49 +0900

    todoを保持するstateであるtodosを設定

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+
+import { useState } from 'react';
+
 import Header from './conponents/Header';
 import Input from './conponents/Input';
 import TodoList from './conponents/TodoList';
 
 function App() {
+  // Todoを保持するstateを作成
+  const [todos, setTodos] = useState();
+
+
   return (
     <div >
       <Header />
