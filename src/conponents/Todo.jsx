9fc8cacaafe94a92f4b95e5ff7be71fbc85e4195
boon2187 <fumiya.mai.ito@gmail.com>
2024-04-02 13:33:27 +0900

    ボタンが押されたら、そのtodoのidを引数にして、deleteTodoが実行されるようにhundleClickを追加

--- src/conponents/Todo.jsx
+++ src/conponents/Todo.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 
 const Todo = ({todo,deleteTodo}) => {
+  // ボタンが押されたらdeleteTodo関数を実行してtodosから削除
+  const hundleClick = () => {
+    deleteTodo(todo.id);
+  }
+
   return (
     <li>
       {todo.task}
-      <button>削除</button>
+      <button onClick={hundleClick}>削除</button>
     </li>
   )
 }
